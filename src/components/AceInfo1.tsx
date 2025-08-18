cdf8b119e4a65efb37e39987ce8cc120e9c85abe
김종완 <fittobe.cs@gmail.com>
2025-08-18 14:40:59 +0900

    철자 및 기타 수정

--- src/components/AceInfo1.tsx
+++ src/components/AceInfo1.tsx
@@ -17,9 +17,9 @@ const AceInfo1: React.FC = () => {
         <div className="flex items-start gap-6">
           <img src="/images/ace_icon_1.png" width={80} height={80}></img>
           <div className="flex items-center h-20">
-            <p className="text-lg text-gray-800 leading-relaxed flex-1 text-center">
+            <p className="text-lg text-gray-800 leading-relaxed flex-1">
               ACE(American Council on Exerise)는 1983년에 설립된 미국의 비영리
-              비트니스 교육 기관입니다.
+              피트니스 교육 기관입니다.
             </p>
           </div>
         </div>
